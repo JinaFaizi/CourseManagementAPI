8b61b124c5f189ab684cac9a82d4253726b53dfe
jinafaizi <faizie.jina@gmail.com>
2026-08-19 10:03:55 +0330

    Implement Interface

---
+++ CourseManagemet.Service/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+using CourseManagemet.Service.Models;
+
+namespace CourseManagemet.Service.Interfaces;
+
+public interface ICourseRepository
+{
+    Course Create(Course course);
+    List<Course> GetCourses();
+    Course? GetById(int id);
+    Course? Update(int id, Course course);
+    bool Delete(int id);
+}

05ee5d66ec802aa38c07902829f081064591a2db
jinafaizi <faizie.jina@gmail.com>
2026-08-29 21:14:39 +0330

    Updating ModelBuilderExtension.cs

--- CourseManagement.Core/Extensions/ModelBuilderExtensions.cs
+++ CourseManagement.Core/Extensions/ModelBuilderExtensions.cs
@@ -1,26 +1,19 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 
 namespace CourseManagement.Core.Extensions;
 
 public static class ModelBuilderExtensions
 {
-    public static void RegisterAllEntitiesFromAssembly<T>(
-        this ModelBuilder modelBuilder)
+    public static void RegisterAllEntitiesFromAssembly<BaseType>(
+        this ModelBuilder modelBuilder, params Assembly[] assemblies)
     {
-        var assembly = typeof(T).Assembly;
+        IEnumerable<Type> types = assemblies.SelectMany(a => a.GetExportedTypes())
+            .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic && typeof(BaseType) .IsAssignableFrom(c));
 
-        var entityTypes = assembly
-            .GetTypes()
-            .Where(type =>
-                type.IsClass &&
-                !type.IsAbstract &&
-                type != typeof(T) &&
-                type.BaseType == typeof(T))
-            .ToList();
-
-        foreach (var entityType in entityTypes)
+        foreach (Type type in types)
         {
-            modelBuilder.Entity(entityType);
+            modelBuilder.Entity(type);
         }
     }
 }
